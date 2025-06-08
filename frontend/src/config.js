ab1d72b5f3ac567c9f6a6d09ebbcbd6bb16a37cc
trixon011 <168625447+trixon011@users.noreply.github.com>
2025-06-08 19:45:53 +0530

    BLAH B

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://heart-disease-prediction-2-1a8c.onrender.com/api'
+    ? 'https://heart-disease-prediction-3-u458.onrender.com/api'
     : 'http://127.0.0.1:5000/api';
 
 export default API_URL;
